d06a48d8cd320c69c29bf49782a279be77eb08a4
Tajimul1113 <tushar.tajimulkabir@gmail.com>
2021-04-20 03:49:52 +0600

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace PhotoDiary
+{
+    public partial class Form1 : Form
+    {
+        string cs = ConfigurationManager.ConnectionStrings["photoGal"].ConnectionString; //database connrcetion string
+        public SqlConnection conn;// start connection
+        SqlCommand cmd;
+        public DataSet ds;// for get the autometic list
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(cs);
+            ds = new DataSet();
+            string query = "select eventName from PhotoDir";
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                adapter.Fill(ds);
+                listBox1.DisplayMember = "eventName";
+                listBox1.DataSource = ds.Tables[0];
+                conn.Close();
+            }
+        }
+
+
+
+        private void ListBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            DataRowView dr = (DataRowView)listBox1.SelectedValue;//table view
+            string imgName;
+            if (dr != null)
+            {
+                imgName = dr["eventName"].ToString();
+            }
+            else
+            {
+                imgName = null;
+            }
+            string query = "select img,evedec,lastdate from PhotoDir where eventName='"+imgName+"'";
+            if (conn.State == ConnectionState.Closed && imgName!=null)
+            {
+                conn.Open();
+                SqlCommand comn = new SqlCommand(query, conn);
+                SqlDataReader reader = comn.ExecuteReader();
+                while (reader.Read())
+                {
+                    byte[] img = (byte[])reader["img"];//convert
+                    MemoryStream ms = new MemoryStream(img);
+                    pictureBox1.Image = new Bitmap(ms);//convert
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    label4.Text = (string)reader["evedec"];//title
+                    label2.Text = imgName;
+                    label5.Text = (string)reader["lastdate"];//date
+                }
+                conn.Close();
+            }  
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2(this);
+            form2.ShowDialog();// open the form 2
+            conn.Open();
+            
+            string query = "select eventName from PhotoDir";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+            //ds.Clear();
+            adapter.Fill(ds);
+            listBox1.DisplayMember = "eventName";
+            listBox1.DataSource = ds.Tables[0];
+            conn.Close();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoDiary
+{
+    public partial class Form2 : Form
+    {
+        Form1 form1 = null;
+        byte[] image = null;
+        string imgLocation;
+        public Form2(Form1 form1)
+        {
+            InitializeComponent();
+            this.form1 = form1;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+
+            openFileDialog1.Title = "Select image to be upload.";
+            openFileDialog1.Filter = "Image Only(*.jpg; *.jpeg; *.gif; *.bmp; *.png)|*.jpg; *.jpeg; *.gif; *.bmp; *.png";
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (openFileDialog1.CheckFileExists)
+                {
+                    imgLocation = openFileDialog1.FileName.ToString();
+                    pictureBox1.Image = new Bitmap(openFileDialog1.FileName);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Upload image.");
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)// form 2 submit button
+        {
+            if (imgLocation == null)
+            {
+                MessageBox.Show("Please select a valid image.");
+            }
+            else
+            {
+                FileStream Stream = new FileStream(imgLocation, FileMode.Open, FileAccess.Read);//
+                byte[] bImageData = new byte[Stream.Length];
+                Stream.Read(bImageData, 0, System.Convert.ToInt32(Stream.Length));// for byte conversion
+                Stream.Close();
+                DateTime time = DateTime.Now;
+                string date1 = time.ToString();
+                string query = "INSERT INTO PhotoDir VALUES(@images,@title,@date,@dec)";
+                form1.conn.Open();
+                SqlCommand cmd = new SqlCommand(query, form1.conn);
+                cmd.Parameters.AddWithValue("@images", bImageData);
+                cmd.Parameters.AddWithValue("@date", date1);
+                cmd.Parameters.AddWithValue("@title", textBox1.Text);
+                cmd.Parameters.AddWithValue("@dec", textBox2.Text);
+                cmd.ExecuteNonQuery();
+                form1.conn.Close();
+                MessageBox.Show("Image uploaded successfully.");
+                form1.ds.Clear();
+                this.Close();
+            }
+        }
+    }
+}
